4a9aea421f8aeecf0c52c15f8c58882ebd216178
tiepolow <82149231+tiepolow@users.noreply.github.com>
2021-08-15 15:09:11 +0100

    Add files via upload

---
+++ medias.py
@@ -0,0 +1,9 @@
+a = int(input('Digite a primeira nota: '))
+b = int(input('Digite a segunda nota: '))
+c = int(input('Digite a terceira nota: '))
+d = int(input('Digite a quarta nota: '))
+x = (float(a + b + c + d) / 4)
+
+print('A média aritmética é', x)
+
+
---
+++ perimetro e area.py
@@ -0,0 +1,7 @@
+valor = input("Digite o valor correspondente ao lado de um quadrado: ")
+lado = float(valor)
+
+x = int(lado * 4)
+y = int(lado * lado)
+
+print("perímetro:",x, "- área",y)
